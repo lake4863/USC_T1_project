a07c0d6e67700abff63eb0757782ec95d037047d
Rick Zhang <365844923qq@gmail.com>
2021-09-22 13:57:36 -0500

    config

---
+++ src/components/Config.js
@@ -0,0 +1,6 @@
+export const IMG_URL = "https://image.tmdb.org/t/p/w500/";
+
+export const BASE_URL = "https://api.themoviedb.org/3/";
+export const MOVIE_TYPE = "movie/top_rated?";
+export const API_KEY = "api_key=e22f46bbf3e67727a81d6598226945a9";
+export const REST = "&language=en-US&page=";
