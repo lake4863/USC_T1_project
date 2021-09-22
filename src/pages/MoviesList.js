fb7e11da6fad8cb9aaa2f51c4200a25b494da048
Rick Zhang <365844923qq@gmail.com>
2021-09-22 18:53:16 -0500

    Day2-UpdateMovieList

--- src/pages/MoviesList.js
+++ src/pages/MoviesList.js
@@ -5,7 +5,11 @@ import { BASE_URL, MOVIE_TYPE, API_KEY, REST } from "../components/Config";
 class MoviesList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loading: true, movie: [], page: 1 };
+    this.state = {
+      loading: true,
+      movie: [],
+      page: 1,
+    };
   }
   async componentDidMount() {
     const url = `${BASE_URL}${MOVIE_TYPE}${API_KEY}${REST}` + this.state.page;
@@ -13,30 +17,21 @@ class MoviesList extends React.Component {
     const data = await response.json();
     this.setState({ movie: data.results, loading: false });
   }
+  async componentDidUpdate() {
+    const url = `${BASE_URL}${MOVIE_TYPE}${API_KEY}${REST}` + this.state.page;
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ movie: data.results, loading: false });
+  }
+
   pre = () => {
-    const Pre = document.getElementById("pre");
-    const Nex = document.getElementById("nex");
-    if (this.state.page == 1) {
-      Pre.disabled = true;
-    }
     if (this.state.page > 1) {
       this.setState({ page: this.state.page - 1 });
-      Pre.disabled = false;
-      Nex.disabled = false;
     }
   };
   nex = () => {
-    const Pre = document.getElementById("pre");
-    const Nex = document.getElementById("nex");
-    if (this.state.page == 500) {
-      Nex.disabled = true;
-      Pre.disabled = false;
-    }
     if (this.state.page < 500) {
       this.setState({ page: this.state.page + 1 });
-      console.log(this.state.page);
-      Nex.disabled = false;
-      Pre.disabled = false;
     }
   };
   render() {
@@ -46,17 +41,26 @@ class MoviesList extends React.Component {
     if (!this.state.movie.length) {
       return <div>Didn't get a movie</div>;
     }
+    const { page, movie } = this.state;
     return (
       <div className="moviesList">
-        <div>
-          <button onClick={this.pre} id="pre">
+        <div className="sortBtn">
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+        <div className="pageBtn">
+          <hr />
+          <button onClick={this.pre} disabled={page === 1 ? true : false}>
             Pre
           </button>
-          <button onClick={this.nex} id="nex">
+          <button onClick={this.nex} disabled={page === 500 ? true : false}>
             Nex
           </button>
+          <hr />
         </div>
-        {this.state.movie.map((movielist) => (
+        {movie.map((movielist) => (
           <div id="Word">
             <img
               src={"https://image.tmdb.org/t/p/w500" + movielist.poster_path}
