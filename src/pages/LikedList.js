bc99c00fa9cc2c467c4d0097a99aca2412b1851c
Rick Zhang <365844923qq@gmail.com>
2021-09-23 01:33:49 -0500

    fix a bug

--- src/pages/LikedList.js
+++ src/pages/LikedList.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { BASE_URL, MOVIE_TYPE, API_KEY, REST } from "../components/Config";
+import {
+  BASE_URL,
+  MOVIE_TYPE,
+  API_KEY,
+  REST,
+  IMG_URL,
+} from "../components/Config";
 
 function LikedList() {
-  const url = `${BASE_URL}${MOVIE_TYPE}${API_KEY}${REST}` + this.state.page;
+  const url = `${BASE_URL}${MOVIE_TYPE}${API_KEY}${REST}1`;
   return (
     <div className="likedList">
       <h1>LikedList</h1>
